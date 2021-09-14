94acc42cd7d2c1cce92f48ae5a8c9b423c32979b
tachikawa <tachikawa_s@beartail.jp>
2021-09-15 02:07:13 +0900

    [add]レイアウトなんとかなりそう

---
+++ src/components/ListItems.tsx
@@ -0,0 +1,40 @@
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import Check from "@material-ui/icons/Check";
+import LayersIcon from "@material-ui/icons/Layers";
+import PeopleIcon from "@material-ui/icons/People";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Link from "next/link";
+import React from "react";
+
+const menus = [
+  {
+    name: "Todo",
+    icon: <Check />,
+    href: "/todo",
+  },
+  {
+    name: "Setting",
+    icon: <PeopleIcon />,
+    href: "/setting",
+  },
+];
+
+export const mainListItems = (
+  <div>
+    {menus.map((menu) => {
+      return (
+        <ListItem key={menu.name} button>
+          <Link href={menu.href}>
+            <>
+              <ListItemIcon>{menu.icon}</ListItemIcon>
+              <ListItemText primary={menu.name} />
+            </>
+          </Link>
+        </ListItem>
+      );
+    })}
+  </div>
+);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "../styles/globals.css";
 import { ThemeProvider as MaterialThemeProvider } from "@material-ui/styles";
 import type { AppProps } from "next/app";
+import React, { useEffect } from "react";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
-import { theme } from "../theme";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
   return (
     <>
       {/* MaterialUIとstyled-componentsに同じthemeを与える */}
