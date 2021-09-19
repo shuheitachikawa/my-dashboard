d54d00d028ff74a5063176c3666373e98b21ed92
tachikawa <tachikawa_s@beartail.jp>
2021-09-19 22:03:41 +0900

    s

---
+++ src/wasm/pkg/wasm_bg.js
@@ -0,0 +1,11 @@
+import * as wasm from './wasm_bg.wasm';
+
+/**
+* @param {number} value
+* @returns {number}
+*/
+export function sums(value) {
+    var ret = wasm.sums(value);
+    return ret;
+}
+
