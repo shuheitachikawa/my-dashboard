21d664b7c154b78e00691598c537ebd67225518a
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 21:22:46 +0900

    s

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Home from './index';
+
+describe('HomePage', () => {
+  it('should render the heading', () => {
+    render(<Home />);
+
+    const heading = screen.queryByText('My Dashboard');
+
+    // we can only use toBeInTheDocument because it was imported
+    // in the jest.setup.js and configured in jest.config.js
+    expect(heading).toBeTruthy();
+  });
+});
