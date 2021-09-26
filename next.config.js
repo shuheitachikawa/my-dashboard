2f6b262a338d22d850693c6890dec74bd559ab74
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 22:54:51 +0900

    s

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  webpack: (config, { isServer }) => {
+  // webpack: (config, { isServer }) => {
     // config.experiments = {
     //   asyncWebAssembly: true
     // };
     // config.output.webassemblyModuleFilename =
     //   (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      };
-    }
-    return config;
-  }
+  // }
 };
