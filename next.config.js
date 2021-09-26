d3a8f6fc6cc2e4d6039272b7c36dd2fd8ac083d2
tachikawa <tachikawa_s@beartail.jp>
2021-09-26 22:31:17 +0900

    s

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  // webpack: (config, { isServer }) => {
-  //   config.experiments = {
-  //     asyncWebAssembly: true
-  //   };
-  //   config.output.webassemblyModuleFilename =
-  //     (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-  //   return config;
-  // }
+  webpack: (config, { isServer }) => {
+    // config.experiments = {
+    //   asyncWebAssembly: true
+    // };
+    // config.output.webassemblyModuleFilename =
+    //   (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      };
+    }
+    return config;
+  }
 };
